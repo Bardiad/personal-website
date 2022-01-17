ab0134e3d7c998f747e1708bed756094e563d0a0
Bardia Doust <bardiad@gmail.com>
2022-01-17 00:08:57 -0500

    More work on the featured project and hero areas of the landing page. Also added packages for svg icon system

--- gulp/tasks/transforms.js
+++ gulp/tasks/transforms.js
@@ -1,1 +1,43 @@
+"use strict";
 
+const { src, dest, series } = require( 'gulp' );
+const svg_sprite = require('gulp-svg-sprite');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+
+const util = require("./common");
+const config = util.getConfig('gulp.config.json');
+
+function _buildSVGSprite(callback) {
+	return src([global.SOURCES_BASE_PATH + "/assets/icons/*.svg"])
+	.pipe(svgmin({
+		js2svg: {
+			pretty: true
+		}
+	}))
+	.pipe(cheerio({
+		run: function($) {
+			$('[fill]').removeAttr('fill');
+			$('[stroke]').removeAttr('stroke');
+			$('[style]').removeAttr('style');
+		},
+		parserOptions: {xmlMode: true}
+	}))
+	.pipe(replace('&gt;', '>'))
+	.pipe(svg_sprite({
+		mode: {
+			symbol: {
+				sprite: global.BUILD_PATH + "img/icon-sprite.svg",
+				render: {
+					scss: {
+						dest:'../../../sass/_sprite.scss',
+						template: assetsDir + "sass/templates/_sprite_template.scss"
+					}
+				}
+			}
+		}		
+	}))
+};
+
+exports.buildSVGSprite = _buildSVGSprite;
